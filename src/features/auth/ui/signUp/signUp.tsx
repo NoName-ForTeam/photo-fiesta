2d39cfb8cb0d7d00ee2bb55038cd2a11b6648db2
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-08-30 18:11:59 +0300

    add signUp form

--- src/features/auth/ui/signUp/signUp.tsx
+++ src/features/auth/ui/signUp/signUp.tsx
@@ -1,29 +1,48 @@
 import { useForm } from 'react-hook-form'
 
+import { GithubSvgrepoCom31, GoogleSvgrepoCom1 } from '@/assets'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, FormCheckbox, FormInput, Typography } from '@photo-fiesta/ui-lib'
 import { z } from 'zod'
 
 import s from './signUp.module.scss'
 
+export const PASSWORD_REGEX =
+  /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_{|}~])[A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\]^_{|}~]+$/
+export const USER_NAME_REGEX = /^[a-zA-Z0-9_-]*$/
+
 const emailSchema = z.string().email()
 
 const signUpSchema = z
   .object({
     agreeWithTerms: z.boolean(),
-    confirmPassword: z.string().min(5),
+    confirmPassword: z.string(),
     email: emailSchema,
-    password: z.string().min(5),
-    username: z.string().min(5),
+    password: z
+      .string()
+      .min(6)
+      .max(20)
+      .regex(PASSWORD_REGEX, 'Minimum number of characters 6,Maximum number of characters 20'),
+    username: z
+      .string()
+      .min(6)
+      .max(30)
+      .regex(USER_NAME_REGEX, 'Minimum number of characters 6,Maximum number of characters 30'),
   })
   .refine(data => data.password === data.confirmPassword, {
-    message: 'Passwords do not match',
+    message: 'Passwords must match',
     path: ['confirmPassword'],
+  })
+  .refine(data => data.agreeWithTerms, {
+    message: '',
+    path: ['agreeWithTerms'],
   })
 
 export type FormValues = z.infer<typeof signUpSchema>
-
-export const SignUp = ({}) => {
+type SignUpProps = {
+  validationError?: string
+}
+export const SignUp = ({ validationError }: SignUpProps) => {
   const { control, handleSubmit } = useForm<FormValues>({
     resolver: zodResolver(signUpSchema),
   })
@@ -33,11 +52,15 @@ export const SignUp = ({}) => {
   return (
     <div className={s.form}>
       <form className={s.formBox} onSubmit={handleSubmit(onSubmit)}>
-        <Typography as={'h1'} className={s.titleSignUp} variant={'h1'}>
+        <Typography className={s.titleSignUp} variant={'h1'}>
           Sign Up
-          <span>
-            {/*<GoogleSvgrepoCom1 />*/}
-            {/*<GithubSvgrepoCom31 />*/}
+          <span className={s.iconsBox}>
+            <Button type={'button'} variant={'link'}>
+              <GoogleSvgrepoCom1 className={s.icon} />
+            </Button>
+            <Button type={'button'} variant={'link'}>
+              <GithubSvgrepoCom31 className={s.icon} />
+            </Button>
           </span>
         </Typography>
         <div className={s.input}>
@@ -49,6 +72,13 @@ export const SignUp = ({}) => {
             type={'username'}
           />
         </div>
+        {validationError && (
+          <div className={s.error}>
+            <Typography as={'span'} variant={'text14'}>
+              {validationError}
+            </Typography>
+          </div>
+        )}
         <div className={s.input}>
           <FormInput
             control={control}
@@ -58,40 +88,65 @@ export const SignUp = ({}) => {
             type={'email'}
           />
         </div>
+        {validationError && (
+          <div className={s.error}>
+            <Typography as={'span'} variant={'text14'}>
+              {validationError}
+            </Typography>
+          </div>
+        )}
         <div className={s.input}>
           <FormInput
             control={control}
             label={'Password'}
             name={'password'}
             placeholder={'jk34!@#GF'}
-            type={'password'}
+            variant={'password'}
           />
         </div>
+        {validationError && (
+          <div className={s.error}>
+            <Typography as={'span'} variant={'text14'}>
+              {validationError}
+            </Typography>
+          </div>
+        )}
         <div className={s.input}>
           <FormInput
             control={control}
             label={'Password Confirmation'}
             name={'confirmPassword'}
             placeholder={'Confirm Password'}
-            type={'password'}
+            variant={'password'}
           />
         </div>
-        <div>
+        {validationError && (
+          <div className={s.error}>
+            <Typography as={'span'} variant={'text14'}>
+              {validationError}
+            </Typography>
+          </div>
+        )}
+        <div className={s.checkbox}>
           <FormCheckbox control={control} name={'agreeWithTerms'} />
-          <span>
+          <Typography variant={'textSmall'}>
             {' '}
             I agree to the <a href={'/terms'}>Terms of Service</a> and{' '}
             <a href={'/privacy'}>Privacy Policy</a>
-          </span>
+          </Typography>
         </div>
         <div className={s.submitBtn}>
           <Button fullWidth type={'submit'}>
             Sign Up
           </Button>
         </div>
-        <div className={s.haveAcc}>Do you have an account?</div>
+        <Typography className={s.haveAcc} variant={'text16'}>
+          Do you have an account?
+        </Typography>
         <div className={s.signIn}>
-          <a>Sign In</a>
+          <Button asChild variant={'link'}>
+            <a href={'/signIn'}>Sign In</a>
+          </Button>
         </div>
       </form>
     </div>
