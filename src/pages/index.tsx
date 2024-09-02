568f4b2e4894164f9812a0b81637fc9a70e8eb60
Emilo067 <gasanovemil467@gmail.com>
2024-09-02 10:43:09 +0300

    refactor: changes after code review

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { SentEmail } from '@/features/auth/ui'
 import { Modal } from '@photo-fiesta/ui-lib'
 
 export default function Home() {
   return (
     <>
       <Modal />
+      <SentEmail />
     </>
   )
 }
