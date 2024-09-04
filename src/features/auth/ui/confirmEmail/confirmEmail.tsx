6a831c9bfa21319e3ec8818143b34fb7c0d7a5a0
Emilo067 <gasanovemil467@gmail.com>
2024-09-04 06:29:49 +0300

    refactor: changes after code review

--- src/features/auth/ui/confirmEmail/confirmEmail.tsx
+++ src/features/auth/ui/confirmEmail/confirmEmail.tsx
@@ -4,6 +4,25 @@ import Image from 'next/image'
 
 import styles from './confirmEmail.module.scss'
 
+/**
+ * `ConfirmEmail` is a React component that displays a confirmation message after a user has verified their email.
+ * It includes a heading, a short message, and a button to navigate to the sign-in page.
+ * Additionally, an image is displayed alongside the button.
+ *
+ * @component
+ * @example
+ * // Usage example:
+ * import { ConfirmEmail } from './ConfirmEmail'
+ *
+ * function App() {
+ *   return (
+ *     <div>
+ *       <ConfirmEmail />
+ *     </div>
+ *   )
+ * }
+ */
+
 export const ConfirmEmail = () => {
   const classNames = {
     button: styles.btn,
