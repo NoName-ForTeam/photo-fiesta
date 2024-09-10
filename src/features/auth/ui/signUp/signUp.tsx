8f0ade74b1617c1973b481faebae8a581ebed29e
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-09-10 11:14:03 +0300

    fix imports

--- src/features/auth/ui/signUp/signUp.tsx
+++ src/features/auth/ui/signUp/signUp.tsx
@@ -1,11 +1,11 @@
-import { GithubSvgrepoCom31, GoogleSvgrepoCom1 } from '@/assets'
 import { SentEmail } from '@/features'
-import { useSignUpForm } from '@/features/auth/ui/signUp/useSignUpForm'
-import { GithubSvgrepoCom31, GoogleSvgrepoCom1 } from '@/shared/assets/icons'
+import { GithubSvgrepoCom31, GoogleSvgrepoCom1 } from '@/shared/assets'
 import { Button, Card, FormCheckbox, FormInput, Typography } from '@photo-fiesta/ui-lib'
 import Link from 'next/link'
 
 import styles from './signUp.module.scss'
+
+import { useSignUpForm } from './useSignUpForm'
 
 export const SignUp = () => {
   const { control, errors, isOpen, onCloseModalHandler, onSubmit, userEmail } = useSignUpForm()
