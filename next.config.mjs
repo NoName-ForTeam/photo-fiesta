b4eea40f12a022b60cbf7b8c25927f88cd88b763
Lelik Jan <21alex7777@gmail.com>
2024-09-27 23:22:55 +0300

    chore: add js doc comments in  next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ /**
+   * Internationalization configuration
+   * @see https://nextjs.org/docs/advanced-features/i18n-routing
+   */
   i18n: {
+    /**
+     * The default locale to use when visiting a non-locale prefixed path
+     * @type {string}
+     */
     defaultLocale: "en",
     locales: ["en", "ru"],
   },
+   /**
+   * Configuration for Next.js Image component
+   * @see https://nextjs.org/docs/api-reference/next/image
+   */
   images: {
     domains: ['staging-it-incubator.s3.eu-central-1.amazonaws.com'],
   },
