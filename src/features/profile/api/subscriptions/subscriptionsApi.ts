141c04c19fc29d97cb015dcccf731ff2ffdd4197
Lelik Jan <21alex7777@gmail.com>
2024-10-05 14:55:51 +0300

    refactor: add js doc comments for subscriptions api

--- src/features/profile/api/subscriptions/subscriptionsApi.ts
+++ src/features/profile/api/subscriptions/subscriptionsApi.ts
@@ -21,20 +21,33 @@ const {
 
 export const subscriptionsApi = baseApi.injectEndpoints({
   endpoints: builder => ({
+    /**
+     * Fetch the cost of available subscription payments.
+     * @returns {Promise<ResponseCostPayment>} The cost of available subscription payments.
+     */
     getCostPayment: builder.query<ResponseCostPayment, void>({
       providesTags: ['Subscriptions'],
       query: () => GET_SUBSCRIPTIONS_COST_PAYMENT,
     }),
+    /**
+     * Fetch the user's current subscription payment.
+     * @returns {Promise<ResponseCurrentPayment>} The user's current subscription payment details.
+     */
     getCurrentPayment: builder.query<ResponseCurrentPayment, void>({
       providesTags: ['Subscriptions'],
       query: () => GET_SUBSCRIPTIONS_CURRENT_PAYMENT,
     }),
-
+    /**
+     * Fetch the user's payment history.
+     * @returns {Promise<ResponseMyPayments>} The user's payment history.
+     */
     getMyPayments: builder.query<ResponseMyPayments, void>({
       providesTags: ['Subscriptions'],
       query: () => GET_MY_PAYMENTS,
     }),
-
+    /**
+     * Cancel the auto-renewal of the user's subscription.
+     */
     postCancelAutoRenewal: builder.mutation<void, void>({
       invalidatesTags: ['Subscriptions'],
       query: () => ({
@@ -42,7 +55,11 @@ export const subscriptionsApi = baseApi.injectEndpoints({
         url: POST_SUBSCRIPTIONS_CANCEL_AUTO_RENEWAL,
       }),
     }),
-
+    /**
+     * Create a new subscription.
+     * @param {RequestSubscription} params - The subscription details.
+     * @returns {Promise<ResponseSubscription>} The response containing the new subscription details.
+     */
     postSubscription: builder.mutation<ResponseSubscription, RequestSubscription>({
       invalidatesTags: ['Subscriptions'],
       query: params => ({
