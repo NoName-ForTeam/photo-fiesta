c299ed4983b6e2ef5d137643244d82c03af7c667
Lelik Jan <21alex7777@gmail.com>
2024-10-20 19:21:59 +0300

    refactor: add logic for pagination in my payments
    - Fix bug with button in profile

--- src/features/profile/ui/settings/myPayments/myPayments.tsx
+++ src/features/profile/ui/settings/myPayments/myPayments.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 import { MyPaymentsList, useGetMyPaymentsQuery } from '@/features'
 import { Loader } from '@/shared/ui'
@@ -19,23 +19,33 @@ const classNames = {
 export const MyPayments = () => {
   const { data: myPayments, isLoading } = useGetMyPaymentsQuery()
 
-  const [option, setOption] = useState('10')
   const [currentPage, setCurrentPage] = useState<number>(1)
-  const [pageSize, setPageSize] = useState<number>(10)
+  const [pageSize, setPageSize] = useState<number>(5)
 
   const options = [
-    { id: 0, title: '10', value: '10' },
-    { id: 1, title: '20', value: '20' },
+    { id: 0, title: '5', value: '5' },
+    { id: 1, title: '10', value: '10' },
     { id: 2, title: '50', value: '50' },
     { id: 3, title: '100', value: '100' },
   ]
 
+  // change value of payments on page
   const handleOptionChange = (value: string) => {
-    setOption(value)
     setPageSize(Number(value))
-  } // change value of payments on page
+    setCurrentPage(1)
+  }
 
-  const handlePageChange = (num: number) => setCurrentPage(num) // change current page in pagination
+  // change current page in pagination
+  const handlePageChange = (num: number) => setCurrentPage(num)
+
+  const paginatedPayments = useMemo(() => {
+    if (!myPayments) {
+      return []
+    }
+    const startIndex = (currentPage - 1) * pageSize
+
+    return myPayments.slice(startIndex, startIndex + pageSize)
+  }, [myPayments, currentPage, pageSize])
 
   if (isLoading) {
     return <Loader />
@@ -45,7 +55,7 @@ export const MyPayments = () => {
     <div className={classNames.root}>
       {myPayments && myPayments.length && (
         <div className={classNames.container}>
-          <MyPaymentsList payments={myPayments} />
+          <MyPaymentsList payments={paginatedPayments} />
           <Pagination
             currentPage={currentPage}
             onChangePage={handlePageChange}
@@ -54,7 +64,7 @@ export const MyPayments = () => {
           >
             {/* @ts-expect-error: fix type in library*/}
             <SelectContainer content={['Show', 'on page']}>
-              <Select onValueChange={handleOptionChange} pagination value={option}>
+              <Select onValueChange={handleOptionChange} pagination value={pageSize.toString()}>
                 {options.map(option => (
                   <SelectItem key={option.id} value={option.value}>
                     {option.title}
