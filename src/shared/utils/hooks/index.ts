4761883276f5ac3fd6b8029413ddbb06cd5188ec
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-12-09 18:41:47 +0300

    delete import

--- src/shared/utils/hooks/index.ts
+++ src/shared/utils/hooks/index.ts
@@ -1,7 +1,6 @@
 export * from './useChangeTitle'
 export * from './useDelayedLoading'
 export * from './useFollowHandler'
-export * from './useLiked'
 export * from './useModal'
 export * from './useTimeAgo'
 export * from './useTranslation'
