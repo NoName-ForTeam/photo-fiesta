678300197f5a1217b0ae171f8a5cde7a8cac2fd8
Emilo067 <gasanovemil467@gmail.com>
2025-02-09 06:23:29 +0300

    add jsDoc for useDebounce hook

--- src/shared/utils/hooks/useDebounce.ts
+++ src/shared/utils/hooks/useDebounce.ts
@@ -1,24 +1,29 @@
 import { useEffect, useState } from 'react'
 
+/**
+ * Custom hook that returns a debounced value.
+ * The value updates only after the specified delay, preventing frequent updates.
+ */
+
 export function useDebounce(value: string, delay: number) {
   // value and delay in ms (1000ms = 1s)
-  // debounced values
   const [debouncedValue, setDebouncedValue] = useState(value)
 
-  useEffect(
-    () => {
-      // Update debounced value after delay
-      const t = setTimeout(() => {
-        setDebouncedValue(value)
-      }, delay)
+  useEffect(() => {
+    if (delay < 0) {
+      console.warn('Delay should not be negative')
 
-      // clean up the timeout after value changes
-      return () => {
-        clearTimeout(t)
-      }
-    },
-    [value, delay] // re-run if value or delay changes
-  )
+      return
+    }
+    // Update debounced value after delay
+    const timeId = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timeId)
+    }
+  }, [value, delay])
 
   return debouncedValue
 }
