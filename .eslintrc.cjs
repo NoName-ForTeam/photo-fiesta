eedc9f7f7f298286b5e54a52420ca58de2648567
EvgeniyaYakunina <Yakoonina@gmail.com>
2025-03-28 18:08:41 +0300

    fix eslint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', '@it-incubator/eslint-config'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
